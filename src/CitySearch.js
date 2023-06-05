dcd3bbd865064aa4e941d90b35ccd8521e2af66d
Wlad <wlad_diaz@yahoo.com>
2023-06-05 13:55:00 -0400

    4.6 fix all passing

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { InfoAlert } from "./Alert";
 
 class CitySearch extends Component {
     constructor() {
@@ -8,30 +7,19 @@ class CitySearch extends Component {
             query: "",
             suggestions: [],
             showSuggestions: undefined,
-            infoText: "",
         };
     }
 
     handleInputChanged = (event) => {
         const value = event.target.value;
-        this.setState({ showSuggestions: true });
         const suggestions = this.props.locations.filter((location) => {
             return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
         });
-        if (suggestions.length === 0) {
-            this.setState({
-                query: value,
-                infoText: 'We can not find the city you are looking for. Please try another city',
-            });
-        } else {
-            return this.setState({
-                query: value,
-                suggestions,
-                infoText: "",
-            });
-        }
+        this.setState({
+            query: value,
+            suggestions,
+        });
     };
-
 
     handleItemClicked = (suggestion) => {
         this.setState({
@@ -45,7 +33,6 @@ class CitySearch extends Component {
     render() {
         return (
             <div className='CitySearch'>
-                <InfoAlert text={this.state.infoText} />
                 <input
                     type='text'
                     className='city'
--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,47 +1,52 @@
 import React, { Component } from 'react';
-import { ErrorAlert } from "./Alert";
 
 
 class NumberOfEvents extends Component {
-    constructor() {
-        super();
-        this.state = {
-            query: 32,
-            errorText: "",
-        };
+    state = {
+        noe: 32,
+        errorText: ''
     }
 
-    handleInputChanged = (event) => {
-        const value = event.target.value;
-        if (value >= 1 || value <= 32) {
+    // componentDidMount() {
+    //     this.setState({ noe: this.props.noe || 32 });
+    // }
+
+    // changeNOE(value) {
+    //     this.setState({ noe: value })
+    // }
+
+    handleInputChanged = (event, props) => {
+        const inputValue = event.target.value;
+        if (inputValue < 0 || inputValue > 32) {
             this.setState({
-                query: value,
-                errorText: "",
-            });
-            this.props.updateEvents(this.props.selectedCity, value);
-        }
-        if (value < 1 || value > 32) {
+                errorText: 'Please select number from 1 to 32'
+            })
+        } else {
+            this.props.updateEvents(null, inputValue);
             this.setState({
-                query: value,
-                errorText: "Please enter a number between 1 to 52",
-            });
+                noe: inputValue,
+                errorText: ''
+            })
         }
     };
 
     render() {
+        const { noe } = this.state;
         return (
-            <div className='numberOfEvents'>
+            <div className="NumberOfEvents">
+                <h3>Number of Events:</h3>
                 <input
-                    type='number'
-                    className='numberOfEvents'
-                    min={1}
-                    max={32}
-                    value={this.state.query}
-                    onChange={this.handleInputChanged}
-                />
-                <ErrorAlert className='errorMessage' text={this.state.errorText} />
+                    className="noe-input"
+                    type="number"
+                    value={noe}
+                    onChange={event => {
+                        // this.changeNOE(event.target.value);
+                        this.handleInputChanged(event);
+                    }}
+                >
+                </input>
             </div>
-        );
+        )
     }
 }
 
