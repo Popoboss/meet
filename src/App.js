f6ecc9e4f5f6c8ccdf7c4a4e7495eaf20fe1b239
Wlad <wlad_diaz@yahoo.com>
2023-06-12 00:06:25 -0400

    Chart not showing 4.10

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from "react";
 import "./App.css";
-import CitySearch from "./CitySearch";
 import EventList from "./EventList";
+import CitySearch from "./CitySearch";
 import NumberOfEvents from "./NumberOfEvents";
-import { extractLocations, getEvents, checkToken, getAccessToken } from './api';
-import "./nprogress.css";
+import { extractLocations, getEvents, checkToken, getAccessToken } from "./api";
 import { WarningAlert } from "./Alert";
-import WelcomeScreen from './WelcomeScreen';
-
+import WelcomeScreen from "./WelcomeScreen";
+import {
+  ScatterChart,
+  Scatter,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
 
 
 class App extends Component {
@@ -15,27 +22,40 @@ class App extends Component {
     events: [],
     locations: [],
     eventCount: 32,
-    selectedCity: 'all',
+    selectedCity: null,
     warningText: "",
-    showWelcomeScreen: undefined
+    showWelcomeScreen: undefined,
   };
+
+  getData = () => {
+    const { locations, events } = this.state;
+    const data = locations.map((location) => {
+      const number = events.filter((event) => event.location === location)
+        .length;
+      const city = location.split(", ").shift();
+      return { city, number };
+    });
+    return data;
+  };
+
   async componentDidMount() {
     this.mounted = true;
-    const accessToken = localStorage.getItem('access_token');
+    const accessToken = localStorage.getItem("access_token");
     const isTokenValid = (await checkToken(accessToken)).error ? false : true;
     const searchParams = new URLSearchParams(window.location.search);
-    const code = searchParams.get("code");
+    const code = await searchParams.get("code");
     this.setState({ showWelcomeScreen: !(code || isTokenValid) });
     if ((code || isTokenValid) && this.mounted) {
       getEvents().then((events) => {
         if (this.mounted) {
-          events = events.slice(0, 32);
-          this.setState({ events, locations: extractLocations(events) });
+          this.setState({
+            events: events,
+            locations: extractLocations(events),
+          });
         }
       });
     }
   }
-
   componentWillUnmount() {
     this.mounted = false;
   }
@@ -43,10 +63,10 @@ class App extends Component {
   promptOfflineWarning = () => {
     if (!navigator.onLine) {
       this.setState({
-        warningText: 'You are offline, events may not be up to date'
-      })
+        warningText: "You are offline, so events may not be up to date",
+      });
     }
-  }
+  };
 
   updateEvents = (location, eventCount) => {
     if (!eventCount) {
@@ -99,32 +119,45 @@ class App extends Component {
   };
 
   render() {
-
-
-    if (this.state.showWelcomeScreen === undefined) return <div className='App' />
+    if (this.state.showWelcomeScreen === undefined)
+      return <div className="App" />;
     return (
-      <div className='App'>
-
-
+      <div className="App">
+        <WelcomeScreen
+          showWelcomeScreen={this.state.showWelcomeScreen}
+          getAccessToken={() => {
+            getAccessToken();
+          }}
+        />
         <CitySearch
           locations={this.state.locations}
           updateEvents={this.updateEvents}
         />
         <NumberOfEvents
-          selectedCity={this.state.selectedCity}
-          query={this.state.eventCount}
+          numberOfEvents={this.state.numberOfEvents}
           updateEvents={this.updateEvents}
         />
-        <WarningAlert text={this.state.warningText} />
+        <h4>Event Capacity Percentage Calculator</h4>
+
+        <h4 className="event-subtitle">Events in each city</h4>
+        <ResponsiveContainer height={400}>
+          <ScatterChart
+            width={400}
+            height={400}
+            margin={{
+              top: 20, right: 20, bottom: 20, left: 20,
+            }}
+          >
+            <CartesianGrid />
+            <XAxis type="category" dataKey="city" name="city" />
+            <YAxis type="number" dataKey="number" name="number of events" allowDecimals={false} />
+            <Tooltip cursor={{ strokeDasharray: "3 3" }} />
+            <Scatter data={this.getData()} fill="#8884d8" />
+          </ScatterChart>
+        </ResponsiveContainer>
 
         <EventList events={this.state.events} />
-
-        <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen}
-          getAccessToken={() => { getAccessToken() }} />
-
-
-
-
+        <WarningAlert text={this.state.offlineText} />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,23 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-//import * as serviceWorker from './serviceWorker';
 import reportWebVitals from './reportWebVitals';
 import * as atatus from 'atatus-spa';
 
-//const root = ReactDOM.createRoot(document.getElementById('root'));
-ReactDOM.render(<App />, document.getElementById('root'));
+atatus.config('440ddde7d18b4a8a9aca6e9fa4ea680c').install();
+
+const root = document.getElementById('root');
+render(
+    <App />, root
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
+//serviceWorkerRegistration.unregister();
+
+// .register() to enable the app work offline 
 serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-atatus.config('2e9b1042165e4bb38f5d5a728feff9da').install();
